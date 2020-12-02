223bca73caf7643f1c0fcacd62556ca6bd22fb2e
tuanh78 <phamtuanh2000@gmail.com>
2020-12-02 19:12:36 +0700

    update 19:12 2/12

---
+++ resources/js/components/TodoList.vue
@@ -0,0 +1,59 @@
+<template>
+    <div id="app">
+        <div class="container todo">
+            <h1 class="todo__heading">
+                <a href="#">ToDoList</a>
+            </h1>
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">First</th>
+                        <th scope="col">Last</th>
+                        <th scope="col">Handle</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">2</th>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                        <td>@fat</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">3</th>
+                        <td>Larry</td>
+                        <td>the Bird</td>
+                        <td>@twitter</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    mounted() {
+        console.log("Component mounted.");
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+.todo {
+    padding-top: 5rem;
+    &__heading {
+        font-size: 3rem;
+        text-align: center;
+        font-style: italic;
+        margin-bottom: 2rem;
+    }
+}
+</style>
